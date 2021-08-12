346c9183e3953501000ef789133c09325c861f54
Pascal Knüppel <captain.p.goldfish@gmx.de>
2021-08-12 19:30:13 +0200

    Add back-button to provider-overview

--- react-frontend/src/openid/openid-provider-overview.js
+++ react-frontend/src/openid/openid-provider-overview.js
@@ -2,6 +2,8 @@ import React from "react";
 import {Tab, Tabs} from "react-bootstrap";
 import OpenidClients from "./openid-clients";
 import ScimClient from "../scim/scim-client";
+import {ArrowLeftCircle} from "react-bootstrap-icons";
+import {LinkContainer} from "react-router-bootstrap";
 
 export default class OpenidProviderOverview extends React.Component
 {
@@ -44,14 +46,26 @@ export default class OpenidProviderOverview extends React.Component
     render()
     {
         return (
-            <Tabs defaultActiveKey="workflow" id="uncontrolled-tab-example">
-                <Tab eventKey="workflow" title="OpenID Workflow">
+            <React.Fragment>
+                <LinkContainer exact
+                               to={"/openIdProvider/"}>
+                    <a>
+                        <h5 style={{height: "35px", padding: "0", paddingLeft: "10px"}}>
+                            <ArrowLeftCircle style={{color: "bisque"}} height={"35px"} size={"25px"} />
+                            <span style={{marginLeft: "15px"}}>Back to Provider Overview</span>
+                        </h5>
+                    </a>
+                </LinkContainer>
 
-                </Tab>
-                <Tab eventKey="clients" title="OpenID Clients">
-                    <OpenidClients provider={this.state.openIdProvider} />
-                </Tab>
-            </Tabs>
+                <Tabs defaultActiveKey="workflow" id="uncontrolled-tab-example">
+                    <Tab eventKey="workflow" title="OpenID Workflow">
+
+                    </Tab>
+                    <Tab eventKey="clients" title="OpenID Clients">
+                        <OpenidClients provider={this.state.openIdProvider} />
+                    </Tab>
+                </Tabs>
+            </React.Fragment>
         )
     }
 }
