99da40f499a22d361b6a86d71be566af3789db25
Pascal Knüppel <captain.p.goldfish@gmx.de>
2022-02-07 22:32:10 +0100

    Update react resources for production build

---
+++ application/src/main/resources/de/captaingoldfish/restclient/application/precache-manifest.c27c2ad53f92438162834a70cb065f9c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f28dcd6c407531eb6fa6a07d4f4634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "247c050d25d21a8305aa",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "e997d8f8fd690c9a7060",
+    "url": "/static/css/main.1e588e85.chunk.css"
+  },
+  {
+    "revision": "247c050d25d21a8305aa",
+    "url": "/static/js/2.cacd4613.chunk.js"
+  },
+  {
+    "revision": "27ec19e9e54c9928030938e142722ea4",
+    "url": "/static/js/2.cacd4613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8eee6db6fff6d7b5567",
+    "url": "/static/js/3.15044897.chunk.js"
+  },
+  {
+    "revision": "e997d8f8fd690c9a7060",
+    "url": "/static/js/main.6d6b0716.chunk.js"
+  },
+  {
+    "revision": "1295b88ddc2730ed8c2e",
+    "url": "/static/js/runtime-main.8c166c41.js"
+  },
+  {
+    "revision": "f8e929aeb8947fca752856ce25574d27",
+    "url": "/static/media/certificate.f8e929ae.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1c809bbb6cf6b8d3725511979428f770",
+    "url": "/static/media/secure-download-icon.1c809bbb.png"
+  }
+]);
